adc408f79096c7b3abe5bb48efc759188455d052
LORDyyyyy <mlord6271@gmail.com>
2024-01-20 18:38:14 +0200

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- a SQL script that creates a table users
---
+-- id, name, email
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
