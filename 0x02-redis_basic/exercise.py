8c2d2c5c49399c8d00682e86ca600dba957f6a6f
LORDyyyyy <mlord6271@gmail.com>
2024-01-24 11:41:27 +0200

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Writing strings to Redis """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache():
@@ -22,3 +22,38 @@ class Cache():
         self._redis.set(id, data)
 
         return (id)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    int,
+                                                                    None]:
+        """
+            take a key string argument
+            and an optional Callable argument named fn.
+            This callable will be used to convert
+            the data back to the desired format.
+        """
+
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn is not None:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ get the string representation from the self.get method """
+        data = self.get(key,
+                        lambda x: x.decode('utf-8')
+                        if isinstance(x, bytes)
+                        else str(x))
+        return str(data)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ get the integer representation from the self.get method """
+        data = self.get(key,
+                        lambda x: int(x) if x.isdigit() else None)
+
+        return data
